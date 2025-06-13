14bea8b813db6d3666a79334b93d175146aece84
kishan <kishan.shr01@gmail.com>
2025-06-13 13:13:44 +0545

    update

--- Todos-Backend/modules/todos/todo.models.js
+++ Todos-Backend/modules/todos/todo.models.js
@@ -13,6 +13,10 @@ const todoSchema = new mongoose.Schema({
     createdBy:{
         type:mongoose.Schema.Types.ObjectId,
         ref:"User"
-    }
+    },
+    subTodos:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"SubTodo"
+    }] //Array of sub-Todos
 },{timestamps:true})
 export const Todo = mongoose.model("Todo",todoSchema)
