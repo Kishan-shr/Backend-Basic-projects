57e6a83c09a61b178c7d691571d0e054408f8a01
kishan <kishan.shr01@gmail.com>
2025-06-17 14:20:52 +0545

    add user and video model

--- project_1_Backend/src/utils/ApiResponse.js
+++ project_1_Backend/src/utils/ApiResponse.js
@@ -2,7 +2,7 @@ class APiResponse{
     constructor(statusCode,data,message = "Success"){
         this.statusCode = statusCode
         this.data = data
-        this,message = message
+        this.message = message
         this.success = statusCode < 400
     }
 }
